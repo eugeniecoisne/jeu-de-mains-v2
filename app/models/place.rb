b0b1efa3f2836d50c294c2097473b71ea6428648
eugenie coisne <coisne.eugenie@gmail.com>
2020-06-01 19:42:20 +0200

    associations and validations added to all created models

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :workshops, dependent: :destroy
+
+  validates :name, :address, :zip_code, :city, :siret_number, presence: true, allow_blank: false
+  validates :name, :siret_number, uniqueness: true
 end
