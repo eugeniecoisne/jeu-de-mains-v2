ef98e7a0d9304b12b8d28f249bb2010881bd9abb
Riwan0807 <53785739+Riwan0807@users.noreply.github.com>
2020-06-02 19:15:17 +0200

    Update place.rb
    Important à rajouter si tu veux pouvoir éditer des plannings pour des Places :)

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :workshops, dependent: :destroy
+  has_many :sessions, through: :workshops
 
   validates :name, :address, :zip_code, :city, :siret_number, presence: true, allow_blank: false
   validates :name, :siret_number, uniqueness: true
