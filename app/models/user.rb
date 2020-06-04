00715c35051ae246b660cfa272b3402555f247cd
Riwan Talmo <riwan.talmo@gmail.com>
2020-06-04 16:00:46 +0200

    Routes ok pour Profile et Place

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,18 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :places, dependent: :destroy
+  after_create :create_profile
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def create_profile
+    @user = User.last
+    @profile = Profile.new
+    @profile.user = @user
+    @profile.save
+  end
 end
