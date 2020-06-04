e736c530f41dbff0071f40426076583337d0811b
eugenie coisne <coisne.eugenie@gmail.com>
2020-06-04 22:59:42 +0200

    navbar, menu, homebanner, config css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,11 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
+require("turbolinks").start()
 require("channels")
 
 import "bootstrap";
+import { initMenu } from '../plugins/init_menu';
+
+document.addEventListener('turbolinks:load', () => {
+  initMenu();
+});
