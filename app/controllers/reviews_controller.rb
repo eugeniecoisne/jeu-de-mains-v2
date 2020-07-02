dc63c52aba63c84784a9a148cc0ccdb154437cfe
eugenie coisne <coisne.eugenie@gmail.com>
2020-07-03 00:15:32 +0200

    select des forms designés, view more reviews avec index de reviews liés à place, profile et workshop faits, pages place et ws éphémères ok, pins map autre couleur, last minute ws cards designées, petites corrections

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i(index)
+  skip_after_action :verify_policy_scoped, :only => :index
 
   def new
     @review = Review.new
@@ -12,9 +14,22 @@ class ReviewsController < ApplicationController
     @review.booking = Booking.find(params[:booking_id])
     @review.user = current_user
     if @review.save
-      redirect_to profile_path(current_user.profile)
+      redirect_to dashboard_profile_path(current_user.profile)
     else
       render 'new'
+    end
+  end
+
+  def index
+    if params[:workshop_id]
+      @workshop = Workshop.find(params[:workshop_id])
+      authorize @workshop
+    elsif params[:place_id]
+      @place = Place.find(params[:place_id])
+      authorize @place
+    elsif params[:profile_id]
+      @profile = Profile.find(params[:profile_id])
+      authorize @profile
     end
   end
 
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -20,7 +20,7 @@ const initMapbox = () => {
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 
-      new mapboxgl.Marker()
+      new mapboxgl.Marker({ "color": "#52828D" })
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(map);
