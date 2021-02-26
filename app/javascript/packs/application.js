ecd47538b053179d216928f8b7b9f220f345d96f
eugenie coisne <coisne.eugenie@gmail.com>
2021-02-26 17:35:21 +0100

    webpacker custom ok + admin can edit ws all attributes even if online

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,24 +28,24 @@ document.addEventListener('turbolinks:load', () => {
   flatpickr(".datepicker", {
     "locale": French,
     altInput: true,
-  });
+  }); // calendars in searches and sessions create
   setTimeout(function() {
     $('.alert').fadeOut();
-  }, 10000);
-  initMenu();
-  initPlaces();
-  initMapbox();
-  initFullMap();
-  initBooking();
-  showMoreWs();
-  initRating();
-  showActions();
-  toggleBtnKpis();
-  prevNextNewWs();
-  showInfomessageBox();
-  showIndexFilters();
-  showGiftcardOptions();
-  submitBookingOptions();
-  submitTrendSelection();
-  initChatroomCable();
+  }, 10000); // alerts (notices and alerts) disappear after 10 seconds
+  initMenu(); // menu burger and side menu in shared#navbar
+  initPlaces(); // Tickets available when booking in workshops#show
+  initMapbox(); // Init Mapbox in workshops#index
+  initFullMap(); // Toggle between map and list in mobile view of workshops#index
+  initBooking(); // Booking button in workshops#show
+  showMoreWs(); // Show more or less workshops in profiles#public
+  initRating(); // Dynamical rating in reviews#new
+  showActions(); // Dashboard my workshops button to manage a ws and show actions in profiles#_dashboard_organizer
+  toggleBtnKpis(); // Toggle KPIS in profiles#_dashboard_organizer
+  prevNextNewWs(); // Prev and Next btns in workshops#new and workshops#finalization
+  showInfomessageBox(); // Show infomessage text editor to send email in sessions#participants
+  showIndexFilters(); // shows filters in profiles#index
+  showGiftcardOptions(); // when using a giftcard in booking#options
+  submitBookingOptions(); // when booking workshop in bookings#options
+  submitTrendSelection(); // go to trends search from pages#home
+  initChatroomCable(); // messages with chatroom and message
 });
