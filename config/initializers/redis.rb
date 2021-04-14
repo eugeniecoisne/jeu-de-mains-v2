9ac91636481de0cc347eaf43e067ac15a7197974
eugenie coisne <coisne.eugenie@gmail.com>
2021-04-14 19:09:00 +0200

    update of postmark gem supposed to be done now

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,11 +4,11 @@ url = ENV["REDISCLOUD_URL"]
 
 if url
   Sidekiq.configure_server do |config|
-    config.redis = { url: url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+    config.redis = { url: url }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+    config.redis = { url: url }
   end
-  $redis = Redis.new(url: url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+  $redis = Redis.new(:url => url)
 end
