088a2a6db1ec2ed0e777713eb050e42a6ee8553d
eugenie coisne <coisne.eugenie@gmail.com>
2021-04-14 18:10:44 +0200

    try to fix ssl error for sidekiq & redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,11 +4,11 @@ url = ENV["REDISCLOUD_URL"]
 
 if url
   Sidekiq.configure_server do |config|
-    config.redis = { url: url }
+    config.redis = { url: url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: url }
+    config.redis = { url: url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
   end
-  $redis = Redis.new(:url => url)
+  $redis = Redis.new(url: url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 end
