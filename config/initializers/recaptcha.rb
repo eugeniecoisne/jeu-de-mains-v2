86d8548b2d60e00e189ab3679d226f2fe800e1ee
eugenie coisne <coisne.eugenie@gmail.com>
2021-04-14 17:20:04 +0200

    added recaptcha to become partner + contact us + entreprises

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,5 @@
 Recaptcha.configure do |config|
-  config.site_key = 'RECAPTCHA_GOOGLE_PUBLIC_KEY'
+  config.site_key = '6LeekqkaAAAAAIEDMUD5ny7e-VrYqkUYklNp6L7C'
   config.secret_key = 'RECAPTCHA_GOOGLE_PRIVATE_KEY'
+  config.proxy = 'http://www.google.com/recaptcha/api/verify'
 end
