28d4f8847234709154899a44cffaba69c69456da
eugenie coisne <coisne.eugenie@gmail.com>
2021-04-30 18:40:34 +0200

    all prep for domain load

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  config.action_mailer.default_url_options = { host: "jeu-de-mains-0121.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "jeudemains.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
